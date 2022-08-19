4bec669234dd84d0e97184e1641d4f25c26246ce
Brandon Nance <bwnance@gmail.com>
2022-08-18 23:12:52 -0400

    progress now

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -9,7 +9,6 @@ MAIN_DIR=${PWD}
 BUILD_DIR=${PWD}/ci_build
 export PATH=${BUILD_DIR}/pru-gcc/bin:${PATH}
 PYTHON=${BUILD_DIR}/python-env/bin/python
-PYTHON2=${BUILD_DIR}/python2-env/bin/python
 
 
 ######################################################################
@@ -28,16 +27,6 @@ finish_test()
     echo "=============== Finished $2"
     echo "::endgroup::"
 }
-
-
-######################################################################
-# Check for whitespace errors
-######################################################################
-
-start_test check_whitespace "Check whitespace"
-./scripts/check_whitespace.sh
-finish_test check_whitespace "Check whitespace"
-
 
 ######################################################################
 # Run compile tests for several different MCU types
@@ -68,14 +57,7 @@ start_test klippy "Test klippy import (Python3)"
 $PYTHON klippy/klippy.py --import-test
 finish_test klippy "Test klippy import (Python3)"
 
-start_test klippy "Test klippy import (Python2)"
-$PYTHON2 klippy/klippy.py --import-test
-finish_test klippy "Test klippy import (Python2)"
 
 start_test klippy "Test invoke klippy (Python3)"
 $PYTHON scripts/test_klippy.py -d ${DICTDIR} test/klippy/*.test
 finish_test klippy "Test invoke klippy (Python3)"
-
-start_test klippy "Test invoke klippy (Python2)"
-$PYTHON2 scripts/test_klippy.py -d ${DICTDIR} test/klippy/*.test
-finish_test klippy "Test invoke klippy (Python2)"
