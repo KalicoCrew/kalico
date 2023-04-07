fe1304a114a1f676bcf2395487b2ccd7b6e70e48
Dmitry Butyugin <dmbutyugin@google.com>
2023-04-07 20:49:23 +0200

    stepper: New optional high precision stepping protocol
    This enables an extended processing of the steps
        next_step = step + (interval + rounding) >> shift;
        interval += add;
        add += add2;
    
    Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -19,25 +19,11 @@ COMPILE_ARGS = (
 )
 SSE_FLAGS = "-mfpmath=sse -msse2"
 SOURCE_FILES = [
-    "pyhelper.c",
-    "serialqueue.c",
-    "stepcompress.c",
-    "itersolve.c",
-    "trapq.c",
-    "pollreactor.c",
-    "msgblock.c",
-    "trdispatch.c",
-    "kin_cartesian.c",
-    "kin_corexy.c",
-    "kin_corexz.c",
-    "kin_delta.c",
-    "kin_deltesian.c",
-    "kin_polar.c",
-    "kin_rotary_delta.c",
-    "kin_winch.c",
-    "kin_extruder.c",
-    "kin_shaper.c",
-    "kin_idex.c",
+    'pyhelper.c', 'serialqueue.c', 'stepcompress.c', 'stepcompress_hp.c',
+    'itersolve.c', 'trapq.c', 'pollreactor.c', 'msgblock.c', 'trdispatch.c',
+    'kin_cartesian.c', 'kin_corexy.c', 'kin_corexz.c', 'kin_delta.c',
+    'kin_deltesian.c', 'kin_polar.c', 'kin_rotary_delta.c', 'kin_winch.c',
+    'kin_extruder.c', 'kin_shaper.c', 'kin_idex.c',
 ]
 DEST_LIB = "c_helper.so"
 OTHER_FILES = [
@@ -53,6 +39,7 @@ defs_stepcompress = """
     };
 
     struct stepcompress *stepcompress_alloc(uint32_t oid);
+    struct stepcompress *stepcompress_hp_alloc(uint32_t oid);
     void stepcompress_fill(struct stepcompress *sc, uint32_t max_error
         , int32_t queue_step_msgtag, int32_t set_next_step_dir_msgtag);
     void stepcompress_set_invert_sdir(struct stepcompress *sc
