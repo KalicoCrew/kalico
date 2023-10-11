5757882502f9d6b25547f744b93897304bcc5fbe
Brandon Nance <bwnance@gmail.com>
2023-10-11 01:53:27 -0400

    flake

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -3,7 +3,6 @@
 # Copyright (C) 2016-2021  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging
 import math
 
 HOMING_START_DELAY = 0.001
