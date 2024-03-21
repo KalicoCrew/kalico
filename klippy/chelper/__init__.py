dmbutyugin/klipper

extruder: Added support for time offset of extruder vs kinematic moves
Signed-off-by: Dmitry Butyugin <dmbutyugin@google.com>

klippy/chelper/__init__.py
@@ -141,7 +141,7 @@ defs_kin_extruder = """
     struct stepper_kinematics *extruder_stepper_alloc(void);
     void extruder_set_pressure_advance(struct stepper_kinematics *sk
         , double linear_velocity, double linear_offset
-        , double linear_advance, double smooth_time);
+        , double linear_advance, double smooth_time, double offset_time);
 """
 
 defs_kin_shaper = """

KalicoCrew/kalico

klippy/chelper/__init__.py
@@ -170,21 +170,7 @@ defs_kin_winch = """
 defs_kin_extruder = """
     struct stepper_kinematics *extruder_stepper_alloc(void);
     void extruder_set_pressure_advance(struct stepper_kinematics *sk
-        , int n_params, double params[], double time_offset);
-    struct pressure_advance_params;
-    double pressure_advance_linear_model_func(double position
-        , double pa_velocity, struct pressure_advance_params *pa_params);
-    double pressure_advance_tanh_model_func(double position
-        , double pa_velocity, struct pressure_advance_params *pa_params);
-    double pressure_advance_recipr_model_func(double position
-        , double pa_velocity, struct pressure_advance_params *pa_params);
-    void extruder_set_pressure_advance_model_func(struct stepper_kinematics *sk
-        , double (*func)(double, double, struct pressure_advance_params *));
-    int extruder_set_shaper_params(struct stepper_kinematics *sk, char axis
-        , int n, double a[], double t[]);
-    int extruder_set_smoothing_params(struct stepper_kinematics *sk, char axis
-        , int n, double a[], double t_sm, double t_offs);
-    double extruder_get_step_gen_window(struct stepper_kinematics *sk);
+        , double pressure_advance, double smooth_time, double time_offset);
 """
 
 defs_kin_shaper = """