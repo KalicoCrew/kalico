dberlin/danger-klipper

Convert klippy to a python package
1. Fix all imports within klippy to be relative, or absolute klippy.mod

2. For compatibility with external `extras` that will probably still use
   the old import names (e.g. a bare `import stepper` instead of
   `from klippy import stepper`), klippy/compat.py hot patches
   `sys.modules` before starting the Printer reactor. Any module
   currently in `sys.modules` under the `klippy.` package namespace
   are inserted a second time with the leading namespace stripped.

   * To keep compatibility with `klippy.xxx` being used,
     `klippy/__init__.py` does `from klippy.printer import *`, ensuring
     all legacy values exist

3. For compatibility with running `python klippy/klippy.py` (every
   current printer's systemd service, as well as any KIAUH install),
   a shim `klippy/klippy.py` is in place that patches `sys.path` before
   importing and calling `klippy.printer.main()`

klippy/__main__.py
@@ -1,0 +1,3 @@
+from .klippy import main
+
+main()

KalicoCrew/kalico

klippy/__main__.py
@@ -1,0 +1,3 @@
+from .printer import main
+
+main()