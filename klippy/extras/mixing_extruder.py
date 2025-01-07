14ebf45047c901fd04ee3600bf430f2f83973507
Rogerio Goncalves <rogerlz@gmail.com>
2025-01-07 18:35:04 -0300

    make ruff happy

--- klippy/extras/mixing_extruder.py
+++ klippy/extras/mixing_extruder.py
@@ -4,42 +4,54 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
-EXTRUSION_FACTOR_THRESHOLD=0.01
+EXTRUSION_FACTOR_THRESHOLD = 0.01
 
 
 # Remove scales which are to close to zero to be used as
 # recipocal of the rotation_distance. Rescale others to compensate.
 def normalize_mixing(mixing):
-    normalized = tuple(0 if m < EXTRUSION_FACTOR_THRESHOLD else m
-                       for m in mixing)
+    normalized = tuple(
+        0 if m < EXTRUSION_FACTOR_THRESHOLD else m for m in mixing
+    )
     s = sum(normalized)
     return tuple(m / s for m in normalized)
 
 
 def mixing_preset(idx, steppers_count):
     if idx == 0:
-        return [1. / steppers_count for p in range(steppers_count)]
+        return [1.0 / steppers_count for p in range(steppers_count)]
     idx = idx - 1
     if idx < steppers_count:
-        return [1. if p == idx else 0. for p in range(steppers_count)]
+        return [1.0 if p == idx else 0.0 for p in range(steppers_count)]
     idx = idx - steppers_count
     if idx < steppers_count:
-        return [0. if p == idx else 1. / (steppers_count - 1)
-                for p in range(steppers_count)]
+        return [
+            0.0 if p == idx else 1.0 / (steppers_count - 1)
+            for p in range(steppers_count)
+        ]
     idx = idx - steppers_count
     if steppers_count == 3:
         if idx < 2 * steppers_count:
-            return [[0. if p == x else (1 + (((p - x) % 3 + y) % 2)) / 3.
-                     for p in range(steppers_count)]
-                    for x in range(steppers_count) for y in (0, 1)][idx]
+            return [
+                [
+                    0.0 if p == x else (1 + (((p - x) % 3 + y) % 2)) / 3.0
+                    for p in range(steppers_count)
+                ]
+                for x in range(steppers_count)
+                for y in (0, 1)
+            ][idx]
         idx = idx - 2 * steppers_count
     elif steppers_count > 3:
         if idx < (steppers_count * (steppers_count - 1) / 2):
-            return [[0. if p == x or p == y else 1. / (steppers_count - 2)
-                     for p in range(steppers_count)]
-                    for x in range(steppers_count)
-                    for y in range(x + 1, steppers_count)][idx]
-    return [1. / steppers_count for p in range(steppers_count)]
+            return [
+                [
+                    0.0 if p == x or p == y else 1.0 / (steppers_count - 2)
+                    for p in range(steppers_count)
+                ]
+                for x in range(steppers_count)
+                for y in range(x + 1, steppers_count)
+            ][idx]
+    return [1.0 / steppers_count for p in range(steppers_count)]
 
 
 class MixingExtruder(object):
@@ -47,28 +59,31 @@ class MixingExtruder(object):
         self.printer = config.get_printer()
         self._activated = False
         self.name = "mixingextruder"
-        self.stepper_names = tuple(e.strip()
-                               for e in config.get('steppers', '').split(",")
-                               if len(e))
+        self.stepper_names = tuple(
+            e.strip() for e in config.get("steppers", "").split(",") if len(e)
+        )
         if not len(self.stepper_names):
-            raise self.printer.config_error(
-                "No steppers configured for mixing")
+            raise self.printer.config_error("No steppers configured for mixing")
         self._extruder_steppers = []
         self._rotation_distances = ()
         self._trapqs = ()
-        self.extruder_name = config.get('extruder_name',
-                                        self.stepper_names[0])
-        self._mixing = self._active_mixing = \
-            mixing_preset(0, len(self.stepper_names))
+        self.extruder_name = config.get("extruder_name", self.stepper_names[0])
+        self._mixing = self._active_mixing = mixing_preset(
+            0, len(self.stepper_names)
+        )
         self._enabled = False
 
-        self.printer.register_event_handler("klippy:connect",
-                                            self._handle_connect)
+        self.printer.register_event_handler(
+            "klippy:connect", self._handle_connect
+        )
         # Register commands
-        gcode = self.printer.lookup_object('gcode')
-        gcode.register_command("SET_MIXING_EXTRUDER",
-                               self.cmd_SET_MIXING_EXTRUDER,
-                               desc=self.cmd_SET_MIXING_EXTRUDER_help)
+        gcode = self.printer.lookup_object("gcode")
+        gcode.register_command(
+            "SET_MIXING_EXTRUDER",
+            self.cmd_SET_MIXING_EXTRUDER,
+            desc=self.cmd_SET_MIXING_EXTRUDER_help,
+        )
+
     def _handle_connect(self):
         if self._activated:
             return
@@ -78,36 +93,44 @@ class MixingExtruder(object):
             if name != "extruder":
                 printer_extruder_name = "extruder_stepper " + name
             extruder_stepper = self.printer.lookup_object(
-                printer_extruder_name).extruder_stepper
+                printer_extruder_name
+            ).extruder_stepper
             self._extruder_steppers.append(
-                (extruder_stepper, extruder_stepper.stepper))
+                (extruder_stepper, extruder_stepper.stepper)
+            )
         self._rotation_distances = tuple(
             stepper.get_rotation_distance()[0]
-            for _, stepper in self._extruder_steppers)
+            for _, stepper in self._extruder_steppers
+        )
         self._trapqs = tuple(
-            stepper.get_trapq()
-            for _, stepper in self._extruder_steppers)
+            stepper.get_trapq() for _, stepper in self._extruder_steppers
+        )
+
     def get_status(self, eventtime):
         return dict(
-            mixing=",".join("%0.1f" % (m * 100.)
-                            for m in self._active_mixing),
-            ticks=",".join("%d" % (stepper.get_mcu_position())
-                           for _, stepper in self._extruder_steppers))
+            mixing=",".join("%0.1f" % (m * 100.0) for m in self._active_mixing),
+            ticks=",".join(
+                "%d" % (stepper.get_mcu_position())
+                for _, stepper in self._extruder_steppers
+            ),
+        )
+
     def get_name(self):
         return self.name
+
     def _init_mixing(self):
         for i, (extruder_stepper, _) in enumerate(self._extruder_steppers):
             extruder_stepper.sync_to_extruder(self.extruder_name)
+
     def _restore_extruder(self):
-        toolhead = self.printer.lookup_object('toolhead')
+        toolhead = self.printer.lookup_object("toolhead")
         toolhead.flush_step_generation()
         for i, (extruder_stepper, _) in enumerate(self._extruder_steppers):
             extruder_stepper.stepper.set_rotation_distance(
                 self._rotation_distances[i]
             )
-            extruder_stepper.stepper.set_trapq(
-                self._trapqs[i]
-            )
+            extruder_stepper.stepper.set_trapq(self._trapqs[i])
+
     def _apply_mixing(self, mixing):
         if mixing and not self._enabled:
             self._init_mixing()
@@ -117,36 +140,46 @@ class MixingExtruder(object):
         if not self._enabled:
             return
         self._active_mixing = normalize_mixing(mixing)
-        for i, (extruder_stepper,
-                stepper) in enumerate(self._extruder_steppers):
+        for i, (extruder_stepper, stepper) in enumerate(
+            self._extruder_steppers
+        ):
             if self._active_mixing[i] > 0 and stepper.get_trapq():
-              extruder_stepper.set_rotation_distance(
-                  self._rotation_distances[i] / self._active_mixing[i])
+                extruder_stepper.set_rotation_distance(
+                    self._rotation_distances[i] / self._active_mixing[i]
+                )
             elif self._active_mixing[i] > 0 and not stepper.get_trapq():
-              extruder_stepper.sync_to_extruder(self.extruder_name)
-              extruder_stepper.set_rotation_distance(
-                  self._rotation_distances[i] / self._active_mixing[i])
+                extruder_stepper.sync_to_extruder(self.extruder_name)
+                extruder_stepper.set_rotation_distance(
+                    self._rotation_distances[i] / self._active_mixing[i]
+                )
             elif self._active_mixing[i] == 0 and stepper.get_trapq():
-              extruder_stepper.sync_to_extruder(None)
+                extruder_stepper.sync_to_extruder(None)
+
     cmd_SET_MIXING_EXTRUDER_help = "Set scale on stepper"
+
     def cmd_SET_MIXING_EXTRUDER(self, gcmd):
-        if (gcmd.get('FACTORS', None) or
-            gcmd.get('PRESET', None) or
-            gcmd.get('ENABLE', None)):
-            factors = gcmd.get('FACTORS', None)
-            preset = gcmd.get_int('PRESET', 0, minval=0)
-            enable = gcmd.get_int('ENABLE', 1, minval=0, maxval=1)
-            ratios = [1 if p == 0 else 0
-                    for p in range(len(self.stepper_names))]
+        if (
+            gcmd.get("FACTORS", None)
+            or gcmd.get("PRESET", None)
+            or gcmd.get("ENABLE", None)
+        ):
+            factors = gcmd.get("FACTORS", None)
+            preset = gcmd.get_int("PRESET", 0, minval=0)
+            enable = gcmd.get_int("ENABLE", 1, minval=0, maxval=1)
+            ratios = [
+                1 if p == 0 else 0 for p in range(len(self.stepper_names))
+            ]
             if factors:
                 try:
-                    factors = [float(s)
-                            for s in factors.replace(',', ':').split(":")]
+                    factors = [
+                        float(s) for s in factors.replace(",", ":").split(":")
+                    ]
                     if len(factors) > len(self.stepper_names):
                         raise Exception("Invalid index")
                 except Exception as e:
                     raise gcmd.error(
-                        "Invalid stepper/motor factor: %s" % (e.message))
+                        "Invalid stepper/motor factor: %s" % (e.message)
+                    )
                 for index, factor in enumerate(factors):
                     ratios[index] = factor
             elif preset >= 0:
@@ -165,18 +198,29 @@ class MixingExtruder(object):
         else:
             mix = "mixing "
             gcmd.respond_info(
-                mix + ("enabled" if self._enabled else "disabled"))
+                mix + ("enabled" if self._enabled else "disabled")
+            )
             gcmd.respond_info(
-                mix + ",".join("%0.1f%%" % (m * 100.)
-                               for m in self._active_mixing))
+                mix
+                + ",".join("%0.1f%%" % (m * 100.0) for m in self._active_mixing)
+            )
             gcmd.respond_info(
-                mix + ",".join("%d" % (stepper.get_mcu_position())
-                               for _, stepper in self._extruder_steppers))
+                mix
+                + ",".join(
+                    "%d" % (stepper.get_mcu_position())
+                    for _, stepper in self._extruder_steppers
+                )
+            )
             gcmd.respond_info(
-                mix + ",".join(extruder_stepper.name
-                    for extruder_stepper, _ in self._extruder_steppers))
+                mix
+                + ",".join(
+                    extruder_stepper.name
+                    for extruder_stepper, _ in self._extruder_steppers
+                )
+            )
 
-'''
+
+"""
 class GradientMixingExtruder(MixingExtruder):
     def __init__(self, config, **kwargs):
         super(GradientMixingExtruder, self).__init__(config, **kwargs)
@@ -311,9 +355,9 @@ class GradientMixingExtruder(MixingExtruder):
                 gcmd.respond_info(
                     grad + "enabled=%s" % (str(self._gradient_enabled)))
 
-'''
+"""
+
 
 def load_config(config):
-    mixingextruder = MixingExtruder(
-        config.getsection('mixing_extruder'))
+    mixingextruder = MixingExtruder(config.getsection("mixing_extruder"))
     return mixingextruder
