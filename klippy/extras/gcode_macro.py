5461bfb4c2d3f1a84b7aadfb09cc6ad7b5092c97
Franklyn Tackitt <git@frank.af>
2025-11-13 14:18:16 -0700

    Python Macros 2.0

--- klippy/extras/gcode_macro.py
+++ klippy/extras/gcode_macro.py
@@ -3,10 +3,15 @@
 # Copyright (C) 2018-2021  Kevin O'Connor <kevin@koconnor.net>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+from __future__ import annotations
 import traceback, logging, ast, copy, json, threading
 import jinja2, math
 import typing
-
+import functools
+import importlib, importlib.metadata, importlib.util
+import inspect
+import pathlib
+from klippy.printer import Printer
 from klippy import configfile
 
 
@@ -277,7 +282,7 @@ class TemplateVariableWrapperPython:
 
 
 class Template:
-    def __init__(self, printer, env, name, script, script_type="gcode") -> None:
+    def __init__(self, printer, env, name, script, script_type="gcode"):
         self.name = name
         self.printer = printer
         self.env = env
@@ -304,10 +309,307 @@ class Template:
             )
 
 
+BlockingResult = typing.TypeVar("BlockingResult")
+
+
+class PythonMacroContext:
+    'The magic "Printer" object for macros'
+
+    status: dict[str, dict[str, typing.Any]]
+    vars: dict[str, typing.Any]
+
+    raw_params: str
+    params: dict[str, str]
+
+    def __init__(self, printer: Printer, name: str, context: dict):
+        self._printer = printer
+        self._gcode = printer.lookup_object("gcode")
+        self._gcode_macro = printer.lookup_object(f"gcode_macro {name}")
+        self._name = name
+
+        self.status = GetStatusWrapperPython(printer)
+        self.vars = TemplateVariableWrapperPython(self._gcode_macro)
+
+        self.raw_params = context.get("raw_params", None)
+        self.params = context.get("params", {})
+
+    def emit(self, gcode: str):
+        "Run GCode"
+        self._gcode.run_gcode_from_command(gcode)
+
+    def wait_while(self, condition: typing.Callable[[], bool]):
+        "Wait while a condition is True"
+
+        def inner(eventtime):
+            return condition()
+
+        self._printer.wait_while(inner)
+
+    def wait_until(self, condition: typing.Callable[[], bool]):
+        "Wait until a condition is True"
+
+        def inner(eventtime):
+            return not condition()
+
+        self._printer.wait_until(condition)
+
+    def wait_moves(self):
+        "Wait until all moves are completed"
+        if self._toolhead is None:
+            self._toolhead = self._printer.lookup_object("toolhead")
+        self._toolhead.wait_moves()
+
+    def blocking(
+        self, function: typing.Callable[[], BlockingResult]
+    ) -> BlockingResult:
+        "Run a blocking task in a thread, waiting for the result"
+        completion = self._printer.get_reactor().completion()
+
+        def run():
+            try:
+                ret = function()
+                completion.complete((False, ret))
+            except Exception as e:
+                completion.complete((True, e))
+
+        t = threading.Thread(target=run, daemon=True)
+        t.start()
+        [is_exception, ret] = completion.wait()
+        if is_exception:
+            raise ret
+        else:
+            return ret
+
+    def sleep(self, timeout: float):
+        "Wait a given number of seconds"
+        reactor = self._printer.get_reactor()
+        deadline = reactor.monotonic() + timeout
+
+        def check(event):
+            return deadline > reactor.monotonic()
+
+        self._printer.wait_while(check)
+
+    def set_gcode_variable(self, macro: str, variable: str, value: typing.Any):
+        "Save a variable to a gcode_macro"
+        macro = self._printer.lookup_object(f"gcode_macro {macro}")
+        macro.variables = {**macro.variables, variable: value}
+
+    def emergency_stop(self, msg: str = "action_emergency_stop"):
+        "Immediately shutdown Kalico"
+        self._printer.invoke_shutdown(f"Shutdown due to {msg}")
+
+    def respond(self, prefix: str, msg: str):
+        "Send a message to the console"
+        self._gcode.respond_raw(f"{prefix} {msg}")
+
+    def respond_info(self, msg: str):
+        "Send a message to the console"
+        self._gcode.respond_info(msg)
+
+    def respond_raw(self, msg: str):
+        self._gcode.respond_raw(msg)
+
+    def raise_error(self, msg):
+        "Raise a G-Code command error"
+        raise self._printer.command_error(msg)
+
+    def call_remote_method(self, method: str, **kwargs):
+        "Call a Kalico webhooks method"
+        webhooks = self._printer.lookup_object("webhooks")
+        try:
+            webhooks.call_remote_method(method, **kwargs)
+        except self._printer.command_error:
+            logging.exception("Remote call error")
+
+
+class PythonMacroTemplate:
+    def __init__(self, config, macro_func):
+        self.printer = config.get_printer()
+
+        self.name = macro_func.__macro_name__
+        self.func = macro_func
+
+    def create_template_context(self):
+        return {}  # Shim
+
+    def run_gcode_from_command(self, context=None):
+        context = PythonMacroContext(self.printer, self.name, context)
+        try:
+            return self.func(context)
+        except self.printer.command_error:
+            raise
+        except Exception as e:
+            raise self.printer.command_error(f"Error in {self.name}: {e}")
+
+    def __call__(self, context=None):
+        return self.run_gcode_from_command(context)
+
+
+macro_function = typing.Callable[
+    typing.Concatenate[PythonMacroContext, ...], None
+]
+
+
+class PythonMacroLoader:
+    def __init__(self, config: configfile.ConfigWrapper):
+        self._printer = config.get_printer()
+        self._config = config
+        self._root_path = pathlib.Path(
+            self._config.printer.get_start_args()["config_file"]
+        ).parent
+
+        self._filename = None
+        self._loaded_macros = None
+
+    def _build_macro_wrapper(self, name, func):
+        sig = inspect.signature(func)
+        if not sig.parameters:
+            raise configfile.error(
+                f"Error when loading macro {self._filename}:{name}."
+                " Macro functions must accept a parameter for the Printer object"
+            )
+
+        param_gcmd, *parameters = sig.parameters.values()
+        if param_gcmd.annotation not in (sig.empty, PythonMacroContext):
+            raise configfile.error(
+                f"Error when loading macro {self._filename}:{name}."
+                " First parameter must be of type Printer"
+            )
+
+        macro_description = name.upper()
+        for paramspec in parameters:
+            if paramspec.annotation not in (
+                paramspec.empty,
+                str,
+                int,
+                bool,
+                float,
+            ):
+                raise configfile.error(
+                    f"Error when loading macro {self._filename}:{name}."
+                    f" Parameter '{paramspec.name}: {paramspec.annotation}' may only be str, int, float, or bool"
+                )
+
+            param_description = paramspec.name.upper() + "="
+            if paramspec.annotation is float:
+                param_description += "1.23"
+            elif paramspec.annotation is int:
+                param_description += "123"
+            elif paramspec.annotation is bool:
+                if paramspec.default is True:
+                    param_description += "0"
+                elif paramspec.default is False:
+                    param_description += "1"
+                else:
+                    param_description += "0|1"
+            if paramspec.default is not paramspec.empty:
+                param_description = "[" + param_description + "]"
+            macro_description += " " + param_description
+
+        docstring = macro_description
+        if func.__doc__:
+            docstring += "\n\n" + func.__doc__
+
+        @functools.wraps(func)
+        def _wrapper(context: PythonMacroContext):
+            kwargs = {}
+            print(context.params)
+            for paramspec in parameters:
+                param_name = paramspec.name.upper()
+
+                if param_name not in context.params:
+                    if paramspec.default is paramspec.empty:
+                        raise context._printer.command_error(
+                            f"{name} requires {param_name}\n", macro_description
+                        )
+                    else:
+                        continue
+
+                value = context.params[param_name]
+
+                # Special case for boolean values
+                if paramspec.annotation is bool:
+                    value = bool(int(value))
+
+                elif callable(paramspec.annotation):
+                    value = paramspec.annotation(value)
+
+                kwargs[paramspec.name] = value
+
+            bound = sig.bind(context, **kwargs)
+            func(*bound.args, **bound.kwargs)
+
+        _wrapper.__macro_name__ = name
+        _wrapper.__filename__ = self._filename
+        _wrapper.__doc__ = macro_description
+
+        return _wrapper
+
+    def _macro_decorator(
+        self,
+        name: str,
+        rename_existing: typing.Optional[str] = None,
+    ) -> typing.Callable[[macro_function], macro_function]:
+        def macro_decorator(func: macro_function) -> macro_function:
+            wrapped_macro = self._build_macro_wrapper(name, func)
+
+            self._register_macro(wrapped_macro, rename_existing)
+
+            return func
+
+        return macro_decorator
+
+    def get_context(self):
+        return {
+            "Printer": PythonMacroContext,
+            "config": self._config,
+            "gcode_macro": self._macro_decorator,
+        }
+
+    def load(self, filename):
+        self._filename = self._root_path / filename
+
+        if not self._filename.exists():
+            raise configfile.error(
+                f"Error loading python macros: {self._filename} does not exist"
+            )
+
+        spec = importlib.util.spec_from_file_location(
+            self._filename.name, self._filename
+        )
+        module = importlib.util.module_from_spec(spec)
+
+        module.__dict__.update(self.get_context())
+        spec.loader.exec_module(module)
+
+    def _register_macro(self, macro_func, rename_existing=None):
+        section = f"gcode_macro {macro_func.__macro_name__}"
+
+        config = self._config.getsection(section)
+        if not config.has_section(section):
+            config.fileconfig.add_section(section)
+            config.fileconfig.set(section, "description", macro_func.__doc__)
+            if rename_existing:
+                config.fileconfig.set(
+                    section, "rename_existing", rename_existing
+                )
+
+        template = PythonMacroTemplate(config, macro_func)
+
+        gcode_macro = self._printer.lookup_object(section, None)
+        if gcode_macro:
+            gcode_macro.template = template
+        else:
+            gcode_macro = GCodeMacro(config, template)
+            self._printer.add_object(section, gcode_macro)
+
+
 # Main gcode macro template tracking
 class PrinterGCodeMacro:
     def __init__(self, config):
         self.printer = config.get_printer()
+        self.config = config
         self.env = jinja2.Environment(
             "{%",
             "%}",
@@ -323,6 +625,16 @@ class PrinterGCodeMacro:
         self.gcode.register_command(
             "RELOAD_GCODE_MACROS", self.cmd_RELOAD_GCODE_MACROS
         )
+
+        self.load_macros_from_python(config)
+
+    def load_macros_from_python(self, config):
+        files = config.getlist("python", [], sep="\n")
+        for file in files:
+            if not file.strip():
+                continue
+            macro_loader = PythonMacroLoader(config)
+            macro_loader.load(file)
 
     def load_template(self, config, option, default=None):
         name = "%s:%s" % (config.get_name(), option)
@@ -371,12 +683,11 @@ class PrinterGCodeMacro:
     def cmd_RELOAD_GCODE_MACROS(self, gcmd):
         pconfig = configfile.PrinterConfig(self.printer)
         new_config = pconfig.read_main_config()
-        for name, obj in self.printer.lookup_objects("gcode_macro"):
-            try:
-                new_section = new_config.getsection(name)
-            except:
-                continue
 
+        for name, obj in self.printer.lookup_objects("gcode_macro "):
+            if not new_config.has_section(name):
+                continue
+            new_section = new_config.getsection(name)
             if name in [
                 s.get_name()
                 for s in new_config.get_prefix_sections("gcode_macro")
@@ -384,6 +695,9 @@ class PrinterGCodeMacro:
                 template = obj.template
                 script_type, new_script = new_section.getscript("gcode")
                 template.reload(script_type, new_script)
+
+        # TODO: Verify this works like I expect
+        self.load_macros_from_python(self.config)
 
 
 def load_config(config):
@@ -396,7 +710,7 @@ def load_config(config):
 
 
 class GCodeMacro:
-    def __init__(self, config):
+    def __init__(self, config, template: Template = None):
         if len(config.get_name().split()) > 2:
             raise config.error(
                 "Name of section '%s' contains illegal whitespace"
@@ -405,8 +719,10 @@ class GCodeMacro:
         name = config.get_name().split()[1]
         self.alias = name.upper()
         self.printer = printer = config.get_printer()
-        gcode_macro = printer.load_object(config, "gcode_macro")
-        self.template = gcode_macro.load_template(config, "gcode")
+        self.template = template
+        if self.template is None:
+            gcode_macro = printer.load_object(config, "gcode_macro")
+            self.template = gcode_macro.load_template(config, "gcode")
         self.gcode = printer.lookup_object("gcode")
         self.rename_existing = config.get("rename_existing", None)
         self.cmd_desc = config.get("description", "G-Code macro")
