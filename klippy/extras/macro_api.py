ea8fb58c8c718c9c21510daa29d6ead2053279cc
Franklyn Tackitt <git@frank.af>
2025-11-14 12:32:06 -0700

    Redo most of the Python API
    Type stubs in kalico.pyi. Copy to your printer_data/config/ for type
    hinting in most editors
    
    See test/klippy/python_macros_2.py for much of the usage

--- klippy/extras/macro_api.py
+++ klippy/extras/macro_api.py
@@ -1,18 +1,24 @@
 from __future__ import annotations
-import math
-import logging
-import threading
+import collections
 import contextlib
-import typing
+import logging
+import math
 import shlex
+import threading
+import typing
+
 from klippy import configfile
 from klippy.gcode import CommandError
 
 if typing.TYPE_CHECKING:
-    from klippy.gcode import GCodeDispatch
-    from klippy.printer import Printer
+    from klippy.extras.fan import PrinterFan
+    from klippy.extras.fan_generic import PrinterFanGeneric
+    from klippy.extras.gcode_macro import GCodeMacro
+    from klippy.extras.gcode_move import GCodeMove
     from klippy.extras.heaters import PrinterHeaters
     from klippy.extras.save_variables import SaveVariables
+    from klippy.gcode import GCodeDispatch
+    from klippy.printer import Printer
 
 
 BlockingResult = typing.TypeVar("BlockingResult")
@@ -20,21 +26,33 @@ BlockingResult = typing.TypeVar("BlockingResult")
 
 class PythonGcodeWrapper:
     def __init__(self, gcode: GCodeDispatch):
-        self.__gcode = gcode
+        self._gcode = gcode
 
     def __getattr__(self, command: str) -> GCodeCommandWrapper:
-        if command.upper() not in self.__gcode.status_commands:
+        if command.upper() not in self._gcode.status_commands:
             raise AttributeError(f"No such GCode command {command!r}")
-        return GCodeCommandWrapper(self.__gcode, command)
+        return GCodeCommandWrapper(self._gcode, command)
 
     def __call__(self, command: str):
-        self.__gcode.run_script_from_command(command)
+        self._gcode.run_script_from_command(command)
+
+    def absolute_movement(self):
+        self._gcode.run_script_from_command("G90")
+
+    def relative_movement(self):
+        self._gcode.run_script_from_command("G91")
+
+    def absolute_extrusion(self):
+        self._gcode.run_script_from_command("M82")
+
+    def relative_extrusion(self):
+        self._gcode.run_script_from_command("M83")
 
 
 class GCodeCommandWrapper:
     def __init__(self, gcode: GCodeDispatch, command: str):
-        self.__gcode = gcode
-        self.__command = command
+        self._gcode = gcode
+        self._command = command
 
     def _serialize_value(self, value):
         if value is True:
@@ -44,7 +62,7 @@ class GCodeCommandWrapper:
         return shlex.quote(str(value))
 
     def format(self, *args, **params):
-        command = [self.__command]
+        command = [self._command]
         if args:
             command.extend(map(str, args))
 
@@ -54,7 +72,7 @@ class GCodeCommandWrapper:
 
             value = self._serialize_value(raw_value)
             if (
-                self.__gcode.is_traditional_gcode(self.__command)
+                self._gcode.is_traditional_gcode(self._command)
                 and len(key) == 1
             ):
                 command.append(f"{key}{value}")
@@ -64,34 +82,194 @@ class GCodeCommandWrapper:
         return " ".join(command)
 
     def __call__(self, *args: str, **params):
-        self.__gcode.run_script_from_command(self.format(*args, **params))
+        self._gcode.run_script_from_command(self.format(*args, **params))
+
+
+class HeatersAPI:
+    def __init__(self, printer: Printer):
+        self._printer = printer
+        self._gcode: GCodeDispatch = printer.lookup_object("gcode")
+
+    ## API for heaters
+    def set_temperature(
+        self, heater_name: str, temp: typing.Optional[float] = None
+    ):
+        "Set the target temperature for a heater"
+        heaters: PrinterHeaters = self._printer.lookup_object("heaters")
+        heaters.set_temperature(heater_name, temp)
+
+    def temperature_wait(
+        self,
+        sensor_name,
+        min_temp: float = -math.inf,
+        max_temp: float = math.inf,
+    ):
+        """
+        Wait for a heater or sensor to reach a temperature
+
+        If no minimum or maximum is given, this will wait for the heater's control loop to settle
+        """
+        heaters: PrinterHeaters = self._printer.lookup_object("heaters")
+
+        if math.isinf(min_temp) and math.isinf(max_temp):
+            heater = heaters.lookup_heater(sensor_name)
+            heaters._wait_for_temperature(heater)
+
+        else:
+            if sensor_name in heaters.heaters:
+                sensor = heaters.lookup_heater(sensor_name)
+            elif sensor_name in heaters.available_sensors:
+                sensor = self._printer.lookup_object(sensor_name)
+            else:
+                raise CommandError(
+                    f"{sensor_name} is not a valid temperature sensor"
+                )
+
+            @self.wait_until
+            def check(eventtime):
+                temp, _ = sensor.get_temp(eventtime)
+                if min_temp <= temp <= max_temp:
+                    return True
+                self._gcode.respond_raw(heaters._get_temp(eventtime))
+                return False
+
+
+class FanAPI:
+    def __init__(self, printer: Printer):
+        self._printer = printer
+
+    def set_speed(self, fan_name: str, speed: float):
+        "Set the speed of a fan"
+        if fan_name == "fan":
+            fan: PrinterFan = self._printer.lookup_object("fan", None)
+        else:
+            fan: PrinterFanGeneric = self._printer.lookup_object(
+                f"fan_generic {fan_name}", None
+            )
+        if not fan:
+            raise CommandError(f"No fan {fan_name} found")
+
+        fan.fan.set_speed_from_command(speed)
+
+
+class MoveAPI:
+    def __init__(self, printer: Printer):
+        self._gcode_move: GCodeMove = printer.lookup_object("gcode_move")
+
+    def __call__(
+        self,
+        x: typing.Optional[float] = None,
+        y: typing.Optional[float] = None,
+        z: typing.Optional[float] = None,
+        e: typing.Optional[float] = None,
+        *,
+        dx: float = 0.0,
+        dy: float = 0.0,
+        dz: float = 0.0,
+        de: float = 0.0,
+        speed: typing.Optional[float] = None,
+    ):
+        """
+        Move to a position
+
+        `speed` is in mm/s and unlike `G1 Fx` only affects this movement.
+        """
+        pos = self._gcode_move.last_position
+        newpos = [
+            (x if x is not None else pos[0]) + dx,
+            (y if y is not None else pos[1]) + dy,
+            (z if z is not None else pos[2]) + dz,
+            (e if e is not None else pos[3]) + de,
+        ]
+        self._gcode_move.move_to(newpos, speed)
+
+    def set_gcode_offset(
+        self,
+        x: typing.Optional[float] = None,
+        y: typing.Optional[float] = None,
+        z: typing.Optional[float] = None,
+        *,
+        dx: float = 0.0,
+        dy: float = 0.0,
+        dz: float = 0.0,
+        move: bool = False,
+        speed: float = None,
+    ):
+        """
+        Set GCode offsets
+
+        `speed` is in mm/s
+        """
+        offsets = self._gcode_move.homing_position
+        x = (x if x is not None else offsets[0]) + dx
+        y = (y if y is not None else offsets[1]) + dy
+        z = (z if z is not None else offsets[2]) + dz
+        self._gcode_move.set_gcode_offset(x, y, z, move=move, speed=speed)
+
+    def set_speed(self, speed: float):
+        "Set the speed for future moves in mm/s"
+        self._gcode_move.set_speed(speed)
+
+    def set_speed_factor(self, speed_factor: float = 1.0):
+        "Set the movement speed factor"
+        self._gcode_move.set_speed_factor(speed_factor)
+
+    def set_extrude_factor(self, extrude_factor: float = 1.0):
+        self._gcode_move.set_extrude_factor(extrude_factor)
 
 
 class PythonMacroContext:
     'The magic "Printer" object for macros'
 
-    status: dict[str, dict[str, typing.Any]]
-    vars: dict[str, typing.Any]
+    _context: list[dict]
 
-    raw_params: str
-    params: dict[str, str]
+    status: GetStatusWrapperPython
+    vars: TemplateVariableWrapperPython
+    saved_vars: SaveVariablesWrapper
 
     gcode: GCodeCommandWrapper
 
-    def __init__(self, printer: Printer, name: str, context: dict):
-        self._printer = printer
-        self._gcode = printer.lookup_object("gcode")
-        self._gcode_macro = printer.lookup_object(f"gcode_macro {name}")
+    move: MoveAPI
+    heaters: HeatersAPI
+
+    def __init__(self, printer: Printer, name: str):
         self._name = name
+        self._printer = printer
+
+        self._gcode: GCodeDispatch = printer.lookup_object("gcode")
+        self._gcode_macro: GCodeMacro = printer.lookup_object(
+            f"gcode_macro {name}"
+        )
 
         self.status = GetStatusWrapperPython(printer)
         self.vars = TemplateVariableWrapperPython(self._gcode_macro)
         self.saved_vars = SaveVariablesWrapper(printer)
 
-        self.raw_params = context.get("raw_params", None)
-        self.params = context.get("params", {})
-
+        self.move = MoveAPI(printer)
+        self.heaters = HeatersAPI(printer)
         self.gcode = PythonGcodeWrapper(self._gcode)
+
+        self._context = []
+
+    @property
+    def raw_params(self) -> str:
+        if not self._context:
+            return ""
+        return self._context[-1].get("rawparams", "")
+
+    @property
+    def params(self) -> dict[str, str]:
+        if not self._context:
+            return {}
+        return self._context[-1].get("params", {})
+
+    @contextlib.contextmanager
+    def _with_context(self, context: dict):
+        self._context.append(context)
+        try:
+            yield self
+        finally:
+            self._context.remove(context)
 
     def wait_while(self, condition: typing.Callable[[], bool]):
         "Wait while a condition is True"
@@ -177,59 +355,7 @@ class PythonMacroContext:
         except self._printer.command_error:
             logging.exception("Remote call error")
 
-    def set_temperature(
-        self, heater_name: str, temp: typing.Optional[float] = None
-    ):
-        "Set the target temperature for a heater"
-        heaters: PrinterHeaters = self._printer.lookup_object("heaters")
-        heaters.set_temperature(heater_name, temp)
-
-    def temperature_wait(
-        self,
-        sensor_name,
-        min_temp: float = -math.inf,
-        max_temp: float = math.inf,
-    ):
-        """
-        Wait for a heater or sensor to reach a temperature
-
-        If no minimum or maximum is given, this will wait for the heater's control loop to settle
-        """
-        heaters: PrinterHeaters = self._printer.lookup_object("heaters")
-
-        if math.isinf(min_temp) and math.isinf(max_temp):
-            heater = heaters.lookup_heater(sensor_name)
-            heaters._wait_for_temperature(heater)
-
-        else:
-            if sensor_name in heaters.heaters:
-                sensor = heaters.lookup_heater(sensor_name)
-            elif sensor_name in heaters.available_sensors:
-                sensor = self._printer.lookup_object(sensor_name)
-            else:
-                self.raise_error(
-                    f"{sensor_name} is not a valid temperature sensor"
-                )
-
-            @self.wait_until
-            def check(eventtime):
-                temp, _ = sensor.get_temp(eventtime)
-                if min_temp <= temp <= max_temp:
-                    return True
-                self.respond_raw(heaters._get_temp(eventtime))
-                return False
-
-    def set_fan_speed(self, fan_name: str, speed: float):
-        "Set the speed of a fan"
-        if fan_name == "fan":
-            fan = self._printer.lookup_object("fan", None)
-        else:
-            fan = self._printer.lookup_object(f"fan_generic {fan_name}", None)
-        if not fan:
-            raise CommandError(f"No fan {fan_name} found")
-
-        fan.fan.set_speed_from_command(speed)
-
+    # TODO: Should this be `move.save_state()`?
     @contextlib.contextmanager
     def save_gcode_state(
         self,
@@ -273,45 +399,45 @@ class TemplateVariableWrapperPython:
 
 class SaveVariablesWrapper:
     def __init__(self, printer: Printer):
-        self.__save_variables: SaveVariables = printer.lookup_object(
+        self._save_variables: SaveVariables = printer.lookup_object(
             "save_variables", None
         )
 
     def __getitem__(self, name):
-        if self.__save_variables is None:
+        if self._save_variables is None:
             raise CommandError("save_variables is not enabled")
-        return self.__save_variables.allVariables[name]
+        return self._save_variables.allVariables[name]
 
     def __setitem__(self, name, value):
-        if self.__save_variables is None:
+        if self._save_variables is None:
             raise CommandError("save_variables is not enabled")
-        self.__save_variables.set_variable(name, value)
+        self._save_variables.set_variable(name, value)
 
     def __contains__(self, name):
         return (
-            self.__save_variables and name in self.__save_variables.allVariables
+            self._save_variables and name in self._save_variables.allVariables
         )
 
     def __iter__(self):
-        yield from iter(self.__save_variables.allVariables)
+        yield from iter(self._save_variables.allVariables)
 
     def items(self):
-        return self.__save_variables.allVariables.items()
+        return self._save_variables.allVariables.items()
 
 
 class GetStatusWrapperPython:
     def __init__(self, printer):
-        self.printer = printer
+        self._printer = printer
 
-    def __getitem__(self, val):
+    def __getitem__(self, val) -> StatusWrapper:
         sval = str(val).strip()
-        po = self.printer.lookup_object(sval, None)
+        po = self._printer.lookup_object(sval, None)
         if po is None or not hasattr(po, "get_status"):
             raise KeyError(val)
-        eventtime = self.printer.get_reactor().monotonic()
-        return po.get_status(eventtime)
+        eventtime = self._printer.get_reactor().monotonic()
+        return StatusWrapper(po.get_status(eventtime))
 
-    def __getattr__(self, val):
+    def __getattr__(self, val) -> StatusWrapper:
         return self.__getitem__(val)
 
     def __contains__(self, val):
@@ -322,14 +448,22 @@ class GetStatusWrapperPython:
         return True
 
     def __iter__(self):
-        for name, obj in self.printer.lookup_objects():
+        for name, obj in self._printer.lookup_objects():
             if hasattr(obj, "get_status"):
                 yield name
 
-    def get(self, key: str, default: configfile.sentinel):
+    def get(self, key: str, default: configfile.sentinel) -> StatusWrapper:
         try:
             return self[key]
         except KeyError:
             if default is not configfile.sentinel:
                 return default
             raise
+
+
+class StatusWrapper(collections.UserDict):
+    def __init__(self, dict):
+        self.data = dict
+
+    def __getattr__(self, name):
+        return self.__getitem__(name)
