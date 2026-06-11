69e241101cb269ed5b0f887af349178fe5ad8ea0
Danila Dergachev <dderg@protonmail.com>
2026-06-11 23:51:34 +0200

    feat(homing): switch phase-stepped TMC5160 to pulse mode around StallGuard trip moves

--- klippy/extras/tmc5160.py
+++ klippy/extras/tmc5160.py
@@ -394,7 +394,9 @@ class TMC5160:
             config, Registers, self.fields, TMC_FREQUENCY
         )
         # Allow virtual pins to be created
-        tmc.TMCVirtualPinHelper(config, self.mcu_tmc)
+        self._virtual_pin_helper = tmc.TMCVirtualPinHelper(
+            config, self.mcu_tmc
+        )
         stepper_name = " ".join(config.get_name().split()[1:])
         if config.has_section(stepper_name):
             stepper_section = config.getsection(stepper_name)
@@ -414,6 +416,7 @@ class TMC5160:
         ):
             _enable_direct_mode(config, stepper_section, self.fields)
             self._phase_stepping = True
+            self._virtual_pin_helper.phase_mode_helper = self
         # Register commands
         current_helper = TMC5160CurrentHelper(
             config,
